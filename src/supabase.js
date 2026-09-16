8977f2d943a5c4db6ad2fc2f713cc788ee159c26
ehealthems-oss <ehealthems@gmail.com>
2026-09-16 10:28:58 -0300

    Fix: usar chave web correta do Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient(
   "https://cfuvbpdkgqnvpxswhexk.supabase.co",
-  "sb_publishable_ltaNA7nnVozoSCOcZIjg"
+  "sb_publishable_PQDjDhiiRsIoEasuCD72Rw_aEMrOlYH"
 )
